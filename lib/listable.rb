dd95d363e028ba14ffa78eba2b124815cc889ccc
KLSimmons <klsimmons88@gmail.com>
2016-05-28 14:41:28 -0400

    Add format_date and format_priority to listable module

--- lib/listable.rb
+++ lib/listable.rb
@@ -2,5 +2,21 @@ module Listable
 
   def format_description(description)
     "#{description}".ljust(30)
-  end  
+  end
+
+  def format_date(start_date, end_date=nil)
+    dates = @start_date.strftime("%D") if @start_date
+    dates << " -- " + @end_date.strftime("%D") if @end_date
+    dates = "N/A" if !dates
+    @due ? @due.strftime("%D") : "No due date"
+    return dates
+  end
+
+  def format_priority
+    value = " ⇧" if @priority == "high"
+    value = " ⇨" if @priority == "medium"
+    value = " ⇩" if @priority == "low"
+    value = "" if !@priority
+    return value
+  end
 end
